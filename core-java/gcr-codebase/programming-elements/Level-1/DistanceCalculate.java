b4525c5c881cd69df53a72fa5aa7d2cdf168cb9b
Nitin Kumar <nitinthainua800@gmail.com>
2026-06-09 16:55:00 +0530

    [nitin].add adding programming-elements questions

---
+++ core-java/gcr-codebase/programming-elements/Level-1/DistanceCalculate.java
@@ -0,0 +1,9 @@
+public class DistanceCalculate {
+    public static void main(String[] args) {
+        double distanceKm = 10.8;
+        double distanceMiles = distanceKm / 1.6;
+
+        System.out.println("The distance " + distanceKm +" km in miles is " + distanceMiles);
+    }
+}
+
---
+++ core-java/gcr-codebase/programming-elements/Level-1/DistanceConverter.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DistanceConverter {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+       
+        System.out.print("Enter distance in feet: ");
+        double distanceInFeet = input.nextDouble();
+
+        
+        double distanceInYards = distanceInFeet / 3.0;
+
+       
+        double distanceInMiles = distanceInYards / 1760.0;
+
+        
+        System.out.println("The distance in yards is " + distanceInYards +
+                           " and the distance in miles is " + distanceInMiles);
+
+        input.close();
+    }
+}
